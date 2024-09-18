818a26ef3491f9c92b7433621f7d12802494dda2
al6894 <115506396+al6894@users.noreply.github.com>
2024-09-18 17:04:03 -0400

    edited main.py to make window open in the center of the screen

--- main.py
+++ main.py
@@ -5,17 +5,24 @@ import os
 def run_batch():
     # Path to the batch file
     batch_file = r"C:\Users\al334\Documents\VSCode\pcCleaner\test.bat"
-    # Run the batch file
     subprocess.run(batch_file, shell=True)
 
 # Create the main window
 root = tk.Tk()
-root.title("Delete %temp% Contents")
+root.title("pcCleaner")
 
-# Set window size
-root.geometry("300x150")
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
 
-# Add a label
+# Calculate position x and y to center the window
+window_width = 400
+window_height = 300
+position_x = int(screen_width/2 - window_width/2)
+position_y = int(screen_height/2 - window_height/2)
+
+# Set the dimensions and position of the window
+root.geometry(f"{window_width}x{window_height}+{position_x}+{position_y}")
+
 label = tk.Label(root, text="Click to delete contents")
 label.pack(pady=10)
 
